7bb460bae53bd4708bbddf7da7449e9d0cde7b29
dabadcuber5 <dabadcuber5@gmail.com>
2023-02-03 10:49:18 -0800

    flake8 is dumb

--- Fixme.py
+++ Fixme.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 def evens(n):
     '''
     Returns a list of even numbers from 0 to n inclusive. Shiver me Timbers!
     '''
-    return list(range(0,n+1,2))
+    return list(range(0, n + 1, 2))
